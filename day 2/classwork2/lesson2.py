0e6ba41f89c86e6026c3abaa91001031680119a4
Hephaesteus266 <delsteeaverion266@gmail.com>
2024-02-05 13:02:37 +0400

    third commit

---
+++ day 2/classwork2/lesson2.py
@@ -0,0 +1,35 @@
+name = "mariam"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "mariam" არის ცვლადისთვის მნიშვნელობა
+
+surname = "surmanidze"
+
+# print (name)
+# პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "mariam" #ეს არის str (string) ტიპის ცვლადი
+age = 17 # წს არის int (integer) მთელი რიცხვი
+height = 184.5 # ეს აროს float ტიპის ცვლად (ათწილადი)
+# boolean (bool) ტიპის ცვლადი
+
+knows_programming = True # True or False
+is_unintelligent = False # snakecase (უბრალოდ წერის ტიპი სტანდარტულად)
+
+isUnintelligent = False # JavaScript type of CamelCase
+
+
+# print ( name + " " + surname )
+
+# სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+# print (name + age)
+
+# print (type(age))
+# print (type(name))
+# print (type(surname))
+# print (type(height))
+# print (type(knows_programming))
+
+print (name + " " + str(age))
+
+#HOMEWORK: create a profile of yourself in one sentence using the info above
---
+++ day 2/homework2/homework2.py
@@ -0,0 +1,7 @@
+greeting = "Greetings ladies and gentlemen and the inbetween!"
+name = "My name is Mariam"
+surname = "Surmanidze"
+age = 17
+height = 170.6
+
+print ( greeting + " " + name + " " + surname + " my given age is " + str (age) + " and my height is " + str(height))
