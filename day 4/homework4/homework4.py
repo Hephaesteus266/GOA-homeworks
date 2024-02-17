f467778c8c7d0bf53b779effe18ebbec7af4a8a6
Hephaesteus266 <delsteeaverion266@gmail.com>
2024-02-17 17:02:38 +0400

    fifth commit

--- day 4/homework4/homework4.py
+++ day 4/homework4/homework4.py
@@ -1,10 +1,10 @@
 pop = 52 #current age
 mom = 49 #current age
-sis = 12 #same
-bro = 23 #same
+sis = 12 #current age
+bro = 23 #current age
 up_two_years = 2
 
-#two years later
+#ages two years later
 A = pop + up_two_years
 B = mom + up_two_years
 C = sis + up_two_years
