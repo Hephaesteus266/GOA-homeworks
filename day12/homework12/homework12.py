aac2a1da5bad119ac0574e9c1fabd7288e904dae
Hephaesteus266 <delsteeaverion266@gmail.com>
2024-03-03 13:45:58 +0400

    twelvth commit

---
+++ day12/homework12/homework12.py
@@ -0,0 +1,34 @@
+""" #2 """
+
+sum = 0
+num = 1 
+
+while num <= 10:
+    if num % 2 == 0:
+        sum += num
+    num += 1
+print(sum)
+
+""" #3 """
+
+numb = 1
+
+while numb <= 20:
+    if numb % 3 == 0 and numb % 5 == 0:
+        print(numb)
+    numb += 1
+
+
+""" #4 """
+
+for i in range(1, 101):
+    if i % 5 == 0:
+        print(i)
+    i += 1
+
+""" #5 """
+
+for div in range(1,21):
+    if div % 6 == 0:
+        print(div)
+    div += 1
